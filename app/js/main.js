39aa730cf4409db838b371de7608bb07b97769ee
webdevw <bec101rpamm@gmail.com>
2020-04-22 13:30:45 +0300

    added sections statistics,modalwindows,adaptation

--- app/js/main.js
+++ app/js/main.js
@@ -4,5 +4,40 @@ $(function(){
         dots: true,
         arrows: true
     });
+
+
+    $(".loadMore").on('click', function (e) {
+        e.preventDefault();
+    });
+    $("#myBtn").on('click', function (e) {
+        e.preventDefault();
+    });
+    
+    
+
+    var modal = document.getElementById('myModal');
+    var btn = document.getElementById("myBtn");
+    var span = document.getElementById("closer");
+
+    btn.onclick = function(){
+        modal.style.display = "block";
+        $('#videoPlayer').attr("src","https://www.youtube.com/embed/tFIBOQqBUs0");
+
+    }
+    span.onclick = function(){
+        modal.style.display = "none";
+            $('#videoPlayer').attr("src"," ");
+    }
+
+    window.onclick = function(event){
+        if (event.target == modal) {
+            modal.style.display = "none";
+            $('#videoPlayer').attr("src"," ");
+
+        }
+    };
+    
+
+    var mixer = mixitup('.projects__inner-box');
   });
-                  
+      
