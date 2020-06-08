e4820e2c252fb801c3439cbc2fed256b5cf03e77
webdevw <bec101rpamm@gmail.com>
2020-06-08 22:59:20 +0300

    eddit little bugs with adaptation

--- app/js/main.js
+++ app/js/main.js
@@ -1,32 +1,32 @@
+
 jQuery(document).ready(function($){
     var $window=$(window),
     $target=$(".header"),
     $h=$target.offset().top;
     $window.on('scroll',function(){
-    var scrollTop=window.pageYOffset||document.documentElement.scrollTop;
-    if(scrollTop>$h){
-    $target.addClass("topchik");
-    return;
-    }else{
-    $target.removeClass("topchik");
-    }
-    return;
+        var scrollTop=window.pageYOffset||document.documentElement.scrollTop;
+        if(scrollTop>$h){
+            $target.addClass("topchik");
+            return;
+        }else{
+            $target.removeClass("topchik");
+        }
+        return;
     });
-    });
+});
 
-    
 
 $(function(){
-
+    
     $('.slider__inner').slick({
         dots: true,
         arrows: true,
         responsive: [
             {
-              breakpoint: 899,
-              settings: {
-                arrows: false
-              }
+                breakpoint: 899,
+                settings: {
+                    arrows: false
+                }
             }
         ]
     });
@@ -39,10 +39,10 @@ $(function(){
         speed: 500,
         responsive: [
             {
-              breakpoint: 1250,
-              settings: {
-                slidesToShow: 2
-              }
+                breakpoint: 1250,
+                settings: {
+                    slidesToShow: 2
+                }
             },
             {
                 breakpoint: 950,
@@ -52,13 +52,13 @@ $(function(){
                 }
             },
             {
-              breakpoint: 845,
-              settings: {
-                slidesToShow: 1,
-                arrows: false,
-              }
+                breakpoint: 845,
+                settings: {
+                    slidesToShow: 1,
+                    arrows: false,
+                }
             }
-          ]
+        ]
     });
     $('.slider-img-inner').slick({
         dots: true,
@@ -68,43 +68,54 @@ $(function(){
         infinite: true,
         speed: 500
     });
-
-
-    $(".loadMore").on('click', function (e) {
-        e.preventDefault();
-    });
-    $("#myBtn").on('click', function (e) {
-        e.preventDefault();
-    });
     
     
-
-    var modal = document.getElementById('myModal');
-    var btn = document.getElementById("myBtn");
-    var span = document.getElementById("closer");
-
-    btn.onclick = function(){
-        modal.style.display = "block";
-        $('#videoPlayer').attr("src","https://www.youtube.com/embed/tFIBOQqBUs0");
-
-    }
-    span.onclick = function(){
-        modal.style.display = "none";
-            $('#videoPlayer').attr("src"," ");
-    }
-
-    window.onclick = function(event){
-        if (event.target == modal) {
-            modal.style.display = "none";
-            $('#videoPlayer').attr("src"," ");
-
-        }
-    };
+    // $(".loadMore").on('click', function (e) {
+    //     e.preventDefault();
+    // });
+    // $("#myBtn").on('click', function (e) {
+    //     e.preventDefault();
+    // });
+    
+    
+    
+    // var modal = document.getElementById('myModal');
+    // var btn = document.getElementById("myBtn");
+    // var span = document.getElementById("closer");
+    
+    // btn.onclick = function(){
+    //     modal.style.display = "block";
+    //     $('#videoPlayer').attr("src","https://www.youtube.com/embed/tFIBOQqBUs0");
+        
+    // }
+    // span.onclick = function(){
+    //     modal.style.display = "none";
+    //     $('#videoPlayer').attr("src"," ");
+    // }
+    
+    // window.onclick = function(event){
+    //     if (event.target == modal) {
+    //         modal.style.display = "none";
+    //         $('#videoPlayer').attr("src"," ");
+            
+    //     }
+    // };
     
     $('.menu__btn').on('click', function(){
         $('.menu__list').slideToggle();
     });
-
+    
+    wow = new WOW(
+        {
+        boxClass:     'wow',      // default
+        animateClass: 'animate__animated', // default
+        offset:       0,          // default
+        mobile:       true,       // default
+        live:         true        // default
+      }
+      )
+      wow.init();
+     new WOW().init();
+    
     var mixer = mixitup('.projects__inner-box');
-  });
-      
+});
--- gulpfile.js
+++ gulpfile.js
@@ -23,7 +23,9 @@ gulp.task('style', function(){
     return gulp.src([
         'node_modules/normalize.css/normalize.css',
         'node_modules/slick-carousel/slick/slick.css',
-        'node_modules/magnific-popup/dist/magnific-popup.css'
+        'node_modules/magnific-popup/dist/magnific-popup.css',
+        'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.css',
+        'node_modules/animate.css/animate.css'
     ])
         .pipe(concat('libs.min.css'))
         .pipe(cssmin())
@@ -34,7 +36,9 @@ gulp.task('script', function(){
     return gulp.src([
         'node_modules/slick-carousel/slick/slick.js',
         'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
-        'node_modules/mixitup/dist/mixitup.js'
+        'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
+        'node_modules/mixitup/dist/mixitup.js',
+        'node_modules/wow.js/dist/wow.js'
     ])
         .pipe(concat('libs.min.js'))
         .pipe(uglify())
